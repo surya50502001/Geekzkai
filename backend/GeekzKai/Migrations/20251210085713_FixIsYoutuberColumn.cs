c4b7798ce3e2b7f565d7381f22e2b29c4e953257
surya <surya@PRIME06>
2025-12-10 14:28:30 +0530

    14:28

---
+++ backend/GeekzKai/Migrations/20251210085713_FixIsYoutuberColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekzKai.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIsYoutuberColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"IsYoutuber\" TYPE BOOLEAN USING \"IsYoutuber\"::BOOLEAN;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
